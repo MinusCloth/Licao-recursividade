a2cfd4432667a14ed96e3ae425abeea2b1c45d6b
Carlos Penna <144754759+MinusCloth@users.noreply.github.com>
2024-08-19 19:02:15 -0300

    Add files via upload

---
+++ aula/src/exercicio1/Main.java
@@ -0,0 +1,21 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+import exercicio3.FuncaoFib;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	    Funcao funcao = new Funcao();
+        int resultado = funcao.hiperfatorial(4);
+        System.out.println("Hiperfatorial: " + resultado);
+		
+		
+		
+		
+		
+	}
+
+}
